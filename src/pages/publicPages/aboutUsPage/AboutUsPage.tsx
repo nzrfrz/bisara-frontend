2ffda3e442a7a2a91112d2b643b0ded01e1c0b8c
sultanahmadhaidir <s.ahmad@students.amikom.ac.id>
2025-12-15 15:07:45 +0700

    tes branch sultan

--- src/pages/publicPages/aboutUsPage/AboutUsPage.tsx
+++ src/pages/publicPages/aboutUsPage/AboutUsPage.tsx
@@ -18,7 +18,7 @@ export const AboutUsPage = () => {
           objectFit: 'cover',
         }}
       >
-        <img src={comingSoonImg} width="100%" height="100%" />
+        {/* <img src={comingSoonImg} width="100%" height="100%" /> */}
       </div>
     </div>
   );
